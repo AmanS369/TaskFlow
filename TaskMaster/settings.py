e95571f0aea2f9d703f2490b5fba205c1e654fd6
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2025-01-03 23:20:58 +0530

    added dotenv

--- TaskMaster/settings.py
+++ TaskMaster/settings.py
@@ -20,7 +20,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+CORS_ALLOWED_ORIGINS =['*']
 # Application definition
 
 INSTALLED_APPS = [
